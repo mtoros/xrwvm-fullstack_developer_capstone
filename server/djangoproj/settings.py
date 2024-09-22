31dc26eaae42d26dfd9ab0de85f9957130c43666
mtoros <mtoros@gmail.com>
2024-09-22 13:13:55 -0400

    project configured

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,8 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  'https://mtoros-8000.theiadockernext-0-labs'
+                 '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+                 'https://mtoros-8000.theiadockernext-1-labs'
                  '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://mtoros-8000.theiadockernext-0-labs-'
+                        'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+                        'https://mtoros-8000.theiadockernext-1-labs-'
                         'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
